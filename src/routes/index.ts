2e1e4817d4298d98f059e1cc558820a2e5a53b45
Vansh Maurya <vanshmaurya17@gmail.com>
2025-05-10 23:12:42 +0530

    Custom

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
-
+router.get("/healthCheck", (req, res) => {
+    
+    res.status(200).json({
+        message: "Welcome to test portal micorservice",
+        status: 200,
+    });
+});
 
 export default router;
