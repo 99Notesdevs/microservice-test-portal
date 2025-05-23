8f4e5ceb698112e2e95b3dbd26332caa7397e04b
Vansh Maurya <vanshmaurya17@gmail.com>
2025-05-23 15:36:43 +0530

    Added auth to test and testSeries as well

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { TestController } from "../controllers/Test";
+import { authenticate } from "../middlewares/authenticate";
+import { authorizeRoles } from "../middlewares/authorizeRoles";
 
 const testRouter = Router();
 
-testRouter.get("/", TestController.getAllTests);
+testRouter.get("/", authenticate, authorizeRoles(["Admin", "User"]), TestController.getAllTests);
 
-testRouter.get("/:id", TestController.getTestById);
+testRouter.get("/:id", authenticate, authorizeRoles(["Admin", "User"]), TestController.getTestById);
 
-testRouter.post("/", TestController.createTest);
+testRouter.post("/", authenticate, authorizeRoles(["Admin"]), TestController.createTest);
 
-testRouter.put("/:id", TestController.updateTest);
+testRouter.put("/:id", authenticate, authorizeRoles(["Admin"]), TestController.updateTest);
 
-testRouter.delete("/:id", TestController.deleteTest);
+testRouter.delete("/:id", authenticate, authorizeRoles(["Admin"]), TestController.deleteTest);
 
 export default testRouter;
--- src/routes/testSeries.ts
+++ src/routes/testSeries.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { TestSeriesController } from "../controllers/TestSeries";
+import { authenticate } from "../middlewares/authenticate";
+import { authorizeRoles } from "../middlewares/authorizeRoles";
 
 const testSeriesRouter = Router();
 
-testSeriesRouter.get("/", TestSeriesController.getAllTestSeries);
+testSeriesRouter.get("/", authenticate, authorizeRoles(["Admin", "User"]), TestSeriesController.getAllTestSeries);
 
-testSeriesRouter.get("/:id", TestSeriesController.getTestSeriesById);
+testSeriesRouter.get("/:id", authenticate, authorizeRoles(["Admin", "User"]), TestSeriesController.getTestSeriesById);
 
-testSeriesRouter.post("/", TestSeriesController.createTestSeries);
+testSeriesRouter.post("/", authenticate, authorizeRoles(["Admin"]), TestSeriesController.createTestSeries);
 
-testSeriesRouter.put("/:id", TestSeriesController.updateTestSeries);
+testSeriesRouter.put("/:id", authenticate, authorizeRoles(["Admin"]), TestSeriesController.updateTestSeries);
 
-testSeriesRouter.delete("/:id", TestSeriesController.deleteTestSeries);
+testSeriesRouter.delete("/:id", authenticate, authorizeRoles(["Admin"]), TestSeriesController.deleteTestSeries);
 
 export default testSeriesRouter;
