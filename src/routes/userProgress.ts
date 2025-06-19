0cd3674c621f2ab3258cef9b425f1c39238c010f
Vansh Maurya <vanshmaurya17@gmail.com>
2025-06-19 12:57:18 +0530

    Minor changes in progress route auth

--- src/routes/userProgress.ts
+++ src/routes/userProgress.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { UserProgressController } from "../controllers/UserProgress";
+import { authenticate } from "../middlewares/authenticate";
+import { authorizeRoles } from "../middlewares/authorizeRoles";
 
 const progressRouter = Router();
 
-progressRouter.get("/:userId", UserProgressController.getUserProgress);
+progressRouter.get("/:userId", authenticate, authorizeRoles(["User"]), UserProgressController.getUserProgress);
 
 export default progressRouter;
