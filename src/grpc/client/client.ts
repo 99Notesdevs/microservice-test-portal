2be0a11915e86f1e984f28c9423a118deaf56ac1
Vansh Maurya <vanshmaurya17@gmail.com>
2025-06-29 14:57:12 +0530

    SAFE

--- src/grpc/client/client.ts
+++ src/grpc/client/client.ts
@@ -2,6 +2,10 @@ import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import path from 'path';
 
+const grpcHost = process.env.GRPC_HOST || 'localhost';
+const grpcPort = process.env.GRPC_PORT || 50051;
+const address = `${grpcHost}:${grpcPort}`;
+
 const AUTH_PROTO_PATH = path.join(__dirname, '../proto/auth.proto');
 const USER_PROTO_PATH = path.join(__dirname, '../proto/user.proto');
 
@@ -11,8 +15,8 @@ const userPackageDefinition = protoLoader.loadSync(USER_PROTO_PATH);
 const authProto = grpc.loadPackageDefinition(authPackageDefinition).Authenticate as any;
 const userProto = grpc.loadPackageDefinition(userPackageDefinition).User as any;
 
-const authClient = new authProto.AuthService(`${process.env.GRPC_HOST}:${process.env.GRPC_PORT}`, grpc.credentials.createInsecure());
-const userClient = new userProto.UserService(`${process.env.GRPC_HOST}:${process.env.GRPC_PORT}`, grpc.credentials.createInsecure());
+const authClient = new authProto.AuthService(address, grpc.credentials.createInsecure());
+const userClient = new userProto.UserService(address, grpc.credentials.createInsecure());
 
 export const getAuthToken = async (token: string): Promise<any> => {
   return new Promise((resolve, reject) => {
