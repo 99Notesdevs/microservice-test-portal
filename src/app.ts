d9db1a1a24a53f271dde5532ae3378bd0d4afa85
Vansh Maurya <vanshmaurya17@gmail.com>
2025-07-02 23:59:16 +0530

    Logs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import main from "./routes/index";
+import { logRequest } from "./middlewares/logsMiddleware";
 
 export const app = express();
 
@@ -25,5 +26,7 @@ app.use(cors({
 // }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(logRequest);
 
 app.use("/api/v1", main);
